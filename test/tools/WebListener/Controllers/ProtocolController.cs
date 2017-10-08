c84121befb8d61b5b2f87fdec0a9a36efcc5628c
Greg Zimmerman <thezim@gmail.com>
2017-10-08 13:52:06 -0400

    Add protocol support for WebListener.

---
+++ test/tools/WebListener/Controllers/ProtocolController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class ProtocolController : Controller
+    {
+        public JsonResult Index()
+        {
+            Hashtable output = new Hashtable
+            {
+                {"Status"      , "FAILED"},
+                {"Description" , "Only valid for HTTPS requests."}
+            };
+            if (HttpContext.Request.IsHttps)
+            {
+                output = new Hashtable {
+                    {"Status"      , "OK"},
+                    {"Protocol"    , Program.Protocol.ToString()}
+                };
+            };
+            return Json(output);
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- test/tools/WebListener/Program.cs
+++ test/tools/WebListener/Program.cs
@@ -18,12 +18,30 @@ namespace mvc
 {
     public class Program
     {
+        public static SslProtocols Protocol;
         public static void Main(string[] args)
         {
-            if (args.Count() != 4)
+            if (args.Count() < 4)
             {
-                System.Console.WriteLine("Required: <CertificatePath> <CertificatePassword> <HTTPPortNumber> <HTTPSPortNumber>");  
-                Environment.Exit(1); 
+                System.Console.WriteLine("Required: <CertificatePath> <CertificatePassword> <HTTPPortNumber> <HTTPSPortNumber> <SslProtocol>");
+                Environment.Exit(1);
+            }
+            if (args.Count() == 5)
+            {
+                if (Enum.TryParse(typeof(SslProtocols), args[4], true, out object o))
+                {
+                    Protocol = (SslProtocols)o;
+                }
+                else
+                {
+                    var names = String.Join(", ", Enum.GetNames(typeof(SslProtocols)).OrderBy(x => x).ToArray());
+                    System.Console.WriteLine("{0} is an invalid ssl protocol. Valid values are {1}", args[4], names);
+                    Environment.Exit(1);
+                }
+            }
+            else
+            {
+                Protocol = SslProtocols.Tls12;
             }
             BuildWebHost(args).Run();
         }
@@ -32,18 +50,18 @@ namespace mvc
             WebHost.CreateDefaultBuilder()
                 .UseStartup<Startup>().UseKestrel(options =>
                 {
-                   options.Listen(IPAddress.Loopback, int.Parse(args[2]));
-                   options.Listen(IPAddress.Loopback, int.Parse(args[3]), listenOptions =>
-                   {
-                       var certificate = new X509Certificate2(args[0], args[1]);
-                       HttpsConnectionAdapterOptions httpsOption = new HttpsConnectionAdapterOptions();
-                       httpsOption.SslProtocols = SslProtocols.Tls12;
-                       httpsOption.ClientCertificateMode = ClientCertificateMode.AllowCertificate;
-                       httpsOption.ClientCertificateValidation = (inCertificate, inChain, inPolicy) => {return true;};
-                       httpsOption.CheckCertificateRevocation = false;
-                       httpsOption.ServerCertificate = certificate;
-                       listenOptions.UseHttps(httpsOption);
-                   });
+                    options.Listen(IPAddress.Loopback, int.Parse(args[2]));
+                    options.Listen(IPAddress.Loopback, int.Parse(args[3]), listenOptions =>
+                    {
+                        var certificate = new X509Certificate2(args[0], args[1]);
+                        HttpsConnectionAdapterOptions httpsOption = new HttpsConnectionAdapterOptions();
+                        httpsOption.SslProtocols = Protocol;
+                        httpsOption.ClientCertificateMode = ClientCertificateMode.AllowCertificate;
+                        httpsOption.ClientCertificateValidation = (inCertificate, inChain, inPolicy) => { return true; };
+                        httpsOption.CheckCertificateRevocation = false;
+                        httpsOption.ServerCertificate = certificate;
+                        listenOptions.UseHttps(httpsOption);
+                    });
                 })
                 .Build();
     }
